6b9453a2713d3d69cd623783cdc19f5e397596b6
MatheusMeloAntiquera <matheus.melo.a@hotmail.com>
2021-04-29 17:44:30 -0300

    Primeira parte - configuração do projeto

---
+++ src/shared/http/errorHandler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import AppError from '@shared/errors/AppError';
+
+export default function (
+  error: Error,
+  _request: Request,
+  response: Response,
+  _next: NextFunction,
+): Response {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+}
